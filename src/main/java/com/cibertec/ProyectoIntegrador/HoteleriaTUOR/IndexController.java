9b013fc9d9d4c792f06cb17405c65054a0f35e5c
Alfredo <alfredoqooo@gmail.com>
2024-05-07 17:49:25 -0500

    front end finalizado

--- src/main/java/com/cibertec/ProyectoIntegrador/HoteleriaTUOR/IndexController.java
+++ src/main/java/com/cibertec/ProyectoIntegrador/HoteleriaTUOR/IndexController.java
@@ -14,4 +14,52 @@ public class IndexController {
     public String QuienesSomos(){
         return "/admin/quienes-somos";
     }
+    @GetMapping("/atractivos-turisticos")
+    public String AtractivosTuristicos(){
+        return "/admin/atractivos-turisticos";
+    }
+    @GetMapping("/gastronomia")
+    public String Gastronomi(){
+        return "/admin/gastronomia";
+    }
+    @GetMapping("/galeria")
+    public  String Galeria(){
+        return "/admin/galeria";
+    }
+    @GetMapping("/videos")
+    public String Videos(){
+        return "/admin/videos";
+    }
+    @GetMapping("/contactenos")
+    public String Contactenos(){
+        return "/admin/contactenos";
+    }
+    @GetMapping("/fortaleza-real-felipe")
+    public String Fortaleza_real_felipe(){
+        return "/admin/fortaleza-real-felipe";
+    }
+
+    @GetMapping("/museo-naval-peru")
+    public String Museo_naval_peru(){
+        return "/admin/museo-naval-peru";
+    }
+
+    @GetMapping("/zona-monumental-callao")
+    public String Zona_monumental_callao(){
+        return "/admin/zona-monumental-callao";
+    }
+
+    @GetMapping("/huaca-el-paraiso")
+    public String Huaca_el_paraiso(){
+        return "/admin/huaca-el-paraiso";
+    }
+    @GetMapping("/museo-submarino-abtao")
+    public String Museo_submarino_abtao(){
+        return "/admin/museo-submarino-abtao";
+    }
+
+    @GetMapping("/humedales-de-ventanilla")
+    public String Humedales_de_ventanilla(){
+        return "/admin/humedales-de-ventanilla";
+    }
 }
